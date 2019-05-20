b632062da260af16bb2ebf40f4f6e2a898be0150
huang <Administrator@WIN-GUP8H90IISQ>
2019-05-20 15:08:46 +0800

    asd啊实打实的

--- src/com/redis/RedisTest.java
+++ src/com/redis/RedisTest.java
@@ -4,6 +4,7 @@ package com.redis;
 public class RedisTest {
 	public static void main(String[] args) {
 		System.out.println("helliiuyo");
+		System.out.println("込込込込");
 		System.out.println("hello");
 	}
 }
