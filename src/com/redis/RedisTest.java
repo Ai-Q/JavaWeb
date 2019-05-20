5c50d5aa53a4fb8c8d8166c949b3c1bff998f2b0
huang <Administrator@WIN-GUP8H90IISQ>
2019-05-20 14:44:36 +0800

    77

--- src/com/redis/RedisTest.java
+++ src/com/redis/RedisTest.java
@@ -3,7 +3,7 @@ package com.redis;
 
 public class RedisTest {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("helliiuyo");
 		System.out.println("hello");
 	}
 }
